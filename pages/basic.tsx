871e9a7977ddea54e724ac7d788c10d199f864a5
DanielPopOut <stephane.tchangang@gmail.com>
2021-09-20 15:49:46 +0200

    Create basic file page

---
+++ pages/basic.tsx
@@ -0,0 +1,47 @@
+import { useRouter } from 'next/dist/client/router';
+
+type CustomImageData = {
+  imageUrl: string;
+  variables: Record<string, string>;
+};
+
+const BasicPage = () => {
+  const router = useRouter();
+  const imageUrl = router.query.imageUrl as string;
+  const variables = router.query as Record<string, string>;
+  const data: CustomImageData = { imageUrl, variables };
+  return (
+    <div>
+      <Template1 {...data} />
+    </div>
+  );
+};
+
+const defaultImage =
+  'https://images.unsplash.com/photo-1593642634402-b0eb5e2eebc9?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=80';
+
+const Template1 = (data: CustomImageData) => {
+  const { name, question } = data.variables;
+  return (
+    <div id="data_to_screenshot">
+      <img width="400" height="300" src={data.imageUrl || defaultImage} />
+      <div style={{ position: 'absolute', top: 0, left: 20 }}>
+        <h1 style={{ marginBottom: 10 }}>Hello {name} !</h1>
+        <div
+          style={{
+            padding: '0 5px',
+            marginLeft: 30,
+            width: 'fit-content',
+            fontWeight: 'bold',
+            boxShadow: '15px 5px white',
+          }}
+        >
+          {question}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+//Autre image = https://images.unsplash.com/photo-1544731612-de7f96afe55f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=80
+export default BasicPage;
