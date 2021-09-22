067693becba941d466e1e521c240daea0fca61ec
Daniel Tchangang <daniel.tchangang@gmail.com>
2021-09-22 19:13:03 +0200

    feat: create BackgroundInput

---
+++ src/modules/form/BackgroundInput.tsx
@@ -0,0 +1,68 @@
+import React, { HTMLAttributes, useEffect, useState } from 'react';
+import {
+  Control,
+  Controller,
+  FieldValues,
+  UseFormRegister,
+} from 'react-hook-form';
+import { UploadFileComponent } from '../../../pages/tests/uploadFileComponent';
+import { Label } from './Label';
+
+export const BackgroundInput = ({
+  register,
+  control,
+  name,
+  label,
+  ...rest
+}: HTMLAttributes<HTMLInputElement> & {
+  name: string;
+  label?: string;
+  register?: UseFormRegister<FieldValues>;
+  control?: Control<FieldValues>;
+}) => {
+  return (
+    <>
+      <Label label={label} />
+      <Controller
+        render={({ field }) => <BackgroundInputBase {...field} />}
+        control={control}
+        name={name}
+        defaultValue={10}
+      />
+      {/* <input {...register(name)} {...rest} /> */}
+    </>
+  );
+};
+type BackgroundInputState = { backgroundUrl: string };
+const BackgroundInputBase = ({
+  value,
+  onChange,
+}: {
+  value: string;
+  onChange: (data) => void;
+}) => {
+  const [state, setState] = useState<BackgroundInputState>({
+    backgroundUrl: '',
+  });
+  useEffect(() => {
+    console.log(value);
+    const matchedValues = ('' + value).match(/url\((.*)\)/);
+    if (matchedValues) {
+      const backgroundUrl = matchedValues[1];
+      setState({ backgroundUrl });
+    }
+    console.log(matchedValues);
+  }, [value]);
+
+  const onChangeFn = (newState: BackgroundInputState) => {
+    setState(newState);
+    onChange(`url(${newState.backgroundUrl})`);
+  };
+
+  return (
+    <UploadFileComponent
+      url={state.backgroundUrl}
+      onChange={(data) => onChangeFn({ backgroundUrl: data })}
+    />
+  );
+};
