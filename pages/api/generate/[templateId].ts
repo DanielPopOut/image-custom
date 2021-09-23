64a6c1fa8caf0b636a56efe226452d33bf1b6204
Daniel Tchangang <daniel.tchangang@gmail.com>
2021-09-23 14:28:45 +0200

    fix: update generate url path

--- pages/api/generate/[templateId].ts
+++ pages/api/generate/[templateId].ts
@@ -1,4 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { ROUTES } from '../../../src/modules/shared/routes/ROUTES';
 const DOWNLOAD_SELECTOR = '.to_download';
 const SCREENSHOT_REMOTE_URL = 'https://screenshot-web-page.vercel.app';
 // const SCREENSHOT_REMOTE_URL = 'http://localhost:3003';
@@ -6,21 +7,23 @@ const SCREENSHOT_REMOTE_URL = 'https://screenshot-web-page.vercel.app';
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'GET') {
     const templateId = req.query.templateId as string;
-    const pdfName = req.query.pdfName as string;
+    const fileName = req.query.fileName as string;
     const urlSearchParams = new URLSearchParams();
     Object.entries(req.query).map(([key, value]) => {
       urlSearchParams.set(key, value as string);
     });
     const host = req.headers.host;
-    const requestToDownloadResume = `https://${host}/builder/${templateId}/test?${urlSearchParams.toString()}`;
+    const requestToDownloadResume =
+      //   'https://google.com' ||
+      `https://${host}${ROUTES.PREVIEW_TEMPLATE_ID(
+        templateId,
+      )}?${urlSearchParams.toString()}`;
+    const screenshotApiUrl = `${SCREENSHOT_REMOTE_URL}/api/img?selector=${DOWNLOAD_SELECTOR}&url=${requestToDownloadResume}`;
 
-    console.log({ requestToDownloadResume });
-    const result = await fetch(
-      `${SCREENSHOT_REMOTE_URL}/api/img?selector=${DOWNLOAD_SELECTOR}&url=${requestToDownloadResume}`,
-    ).then((res) => res.body);
-    const fileName = pdfName;
-    res.setHeader('Content-disposition', 'attachment; filename=' + fileName);
-    res.setHeader('Content-type', 'image/jpeg');
+    console.log({ requestToDownloadResume, screenshotApiUrl });
+    const result = await fetch(screenshotApiUrl).then((res) => res.body);
+    res.setHeader('Content-Type', 'image/jpeg');
+    res.setHeader('Content-Disposition', `inline;filename=${fileName}.png`);
     res.statusCode = 200;
     res.send(result);
   } else {
