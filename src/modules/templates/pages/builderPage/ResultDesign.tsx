d7173ce3e6e4b024ecfa85bd681fde550dee0232
Daniel Tchangang <daniel.tchangang@gmail.com>
2021-09-26 12:34:06 +0200

    refacto: create DrawingPage

--- src/modules/templates/pages/builderPage/ResultDesign.tsx
+++ src/modules/templates/pages/builderPage/ResultDesign.tsx
@@ -1,9 +1,9 @@
 import React, {
   CSSProperties,
   DragEvent,
+  HTMLAttributes,
   useCallback,
   useContext,
-  useState,
 } from 'react';
 import { Template } from '../../models/template.model';
 import { DraggableTextItem } from './components/basics/TextItem';
@@ -23,33 +23,10 @@ export const ResultDesign = ({
     update: Partial<{ value: string; style: CSSProperties }>,
   ) => void;
 }) => {
-  const [pageElementProps, setNodeElementProps] = useState<HTMLDivElement>();
-
-  const { updateSheetData } = useContext(PageContext);
-
-  const onRefChange = useCallback(
-    (node: HTMLDivElement) => {
-      if (node === null) {
-        // DOM node referenced by ref has been unmounted
-      } else {
-        setNodeElementProps(node);
-        const mainPageRect = node.getBoundingClientRect();
-        updateSheetData({
-          left: mainPageRect.left,
-          top: mainPageRect.top,
-          width: mainPageRect.width,
-          height: mainPageRect.height,
-        });
-      }
-    },
-    [state],
-  ); // adjust deps
-
   const updateItemPositionOnDragEnd = (
     itemId: string,
     dragEvent: DragEvent<HTMLDivElement>,
   ) => {
-    const mainPageRect = pageElementProps.getBoundingClientRect();
     updateElement(itemId, {
       style: {
         top: dragEvent.clientY,
@@ -60,14 +37,8 @@ export const ResultDesign = ({
 
   return (
     <div style={{ border: '1px solid black', width: 'fit-content' }}>
-      <div
-        ref={onRefChange}
-        className='to_download'
-        style={{
-          backgroundColor: 'white',
-          position: 'relative',
-          ...state.page,
-        }}
+      <DrawingPage
+        style={state.page}
         onClick={() => {
           setItemToUpdate(null);
         }}
@@ -87,7 +58,45 @@ export const ResultDesign = ({
             />
           );
         })}
-      </div>
+      </DrawingPage>
+    </div>
+  );
+};
+
+const DrawingPage: React.FC<HTMLAttributes<HTMLDivElement>> = ({
+  children,
+  style,
+  onClick,
+}) => {
+  const { updateSheetData } = useContext(PageContext);
+  const onRefChange = useCallback(
+    (node: HTMLDivElement) => {
+      if (node === null) {
+        // DOM node referenced by ref has been unmounted
+      } else {
+        const mainPageRect = node.getBoundingClientRect();
+        updateSheetData({
+          left: mainPageRect.left,
+          top: mainPageRect.top,
+          width: mainPageRect.width,
+          height: mainPageRect.height,
+        });
+      }
+    },
+    [style],
+  ); // adjust deps
+  return (
+    <div
+      ref={onRefChange}
+      className='to_download'
+      style={{
+        backgroundColor: 'white',
+        position: 'relative',
+        ...style,
+      }}
+      onClick={onClick}
+    >
+      {children}
     </div>
   );
 };
