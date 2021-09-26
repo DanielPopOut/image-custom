9a141927ebe54c58cda33340820cd5a810ca8518
Daniel Tchangang <daniel.tchangang@gmail.com>
2021-09-26 14:43:46 +0200

    fix: load light font also

--- src/modules/form/FontSelector.tsx
+++ src/modules/form/FontSelector.tsx
@@ -40,7 +40,7 @@ export const FontSelect = ({
             apiKey={process.env.NEXT_PUBLIC_WEB_FONT_API_KEY}
             activeFontFamily={field.value}
             onChange={(nextFont) => field.onChange(nextFont.family)}
-            variants={['regular', 'italic', '700']}
+            variants={['regular', 'italic', '300', '700']}
           />
         )}
       />
