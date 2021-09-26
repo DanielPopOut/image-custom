87afe0152264cf78020b11ec8c1565443305e7f7
Daniel Tchangang <daniel.tchangang@gmail.com>
2021-09-26 12:24:11 +0200

    feat: create getDefaultText function

--- src/modules/templates/pages/builderPage/defaultInitialData.tsx
+++ src/modules/templates/pages/builderPage/defaultInitialData.tsx
@@ -1,21 +1,36 @@
 import { TextItemProps } from '../../models/template.model';
 
 const initialPageWidth = 400;
+
+export const getDefaultText = (data: {
+  text: string;
+  top: number;
+  left: number;
+}) => {
+  return {
+    type: 'text',
+    value: data.text,
+    style: {
+      display: 'flex',
+      top: data.top,
+      left: data.left,
+      width: 300,
+      height: 100,
+      color: 'black',
+    },
+  };
+};
+
 export const defaultInitialData = {
   page: { width: '400px', height: '300px' },
   elements: {
     text1: {
       id: 'text1',
-      type: 'text',
-      value: 'Hello Daniel',
-      style: {
-        display: 'flex',
+      ...getDefaultText({
+        text: 'Hello boy',
         top: initialPageWidth / 2,
         left: initialPageWidth / 2,
-        width: 300,
-        height: 100,
-        color: 'black',
-      },
+      }),
     } as TextItemProps,
   },
 };
