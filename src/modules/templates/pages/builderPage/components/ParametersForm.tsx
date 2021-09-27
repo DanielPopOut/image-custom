f321a9b6cb5e0453e0702f40ba3c26df4ce82848
Daniel Tchangang <daniel.tchangang@gmail.com>
2021-09-27 10:52:22 +0200

    refacto: move ParametersForm to its own file

---
+++ src/modules/templates/pages/builderPage/components/ParametersForm.tsx
@@ -0,0 +1,108 @@
+import { CSSProperties } from 'react';
+import { BackgroundInput } from '../../../../form/BackgroundInput';
+import { DimensionInput } from '../../../../form/DimensionInput';
+import { FontSelect } from '../../../../form/FontSelector';
+import Form from '../../../../form/Form';
+import { Input } from '../../../../form/Input';
+import { Select } from '../../../../form/Select';
+import { Template } from '../../../models/template.model';
+
+export const ParametersForm = ({
+  defaultValues,
+  onSubmit,
+}: {
+  defaultValues?;
+  onSubmit: (data) => void;
+}) => {
+  return (
+    <Form<Template['page']>
+      defaultValues={defaultValues}
+      onSubmit={(data) => {
+        onSubmit(data);
+      }}
+      style={containerStyle}
+    >
+      <h4 style={{ marginTop: 0 }}>Page parameters</h4>
+      <DimensionInput name='width' label='width' />
+      {/* <Input name='width' label='width'></Input> */}
+      <DimensionInput name='height' label='height' />
+
+      <BackgroundInput name='background' />
+
+      <Input label='Background color' name='backgroundColor' type='color' />
+      <Select
+        label='Background size'
+        name='backgroundSize'
+        options={['auto', 'contain', 'cover']}
+      />
+      <Select
+        label='Background repeat'
+        name='backgroundRepeat'
+        options={['no-repeat', 'repeat-x', 'repeat-y', 'round']}
+      />
+      <Select
+        label='Background size'
+        name='backgroundPosition'
+        options={['center', 'left', 'right', 'top', 'bottom', 'unset']}
+      />
+      {/* <Input name='height' label='height'></Input> */}
+    </Form>
+  );
+};
+const containerStyle: CSSProperties = {
+  display: 'flex',
+  flexDirection: 'column',
+  maxWidth: 250,
+  padding: 20,
+  border: '1px solid grey',
+};
+const TextInputFormFields = ({
+  defaultValues,
+  onSubmit,
+}: {
+  defaultValues?;
+  onSubmit: (data) => void;
+}) => {
+  return (
+    <Form
+      defaultValues={defaultValues}
+      onChange={({ top, left, ...data }) => {
+        onSubmit(data);
+      }}
+      onSubmit={({ top, left, ...data }) => {
+        onSubmit(data);
+      }}
+      style={{ display: 'flex', flexDirection: 'column', maxWidth: 200 }}
+    >
+      <FontSelect name='fontFamily' label='font' />
+      <DimensionInput name='width' label='width' />
+      <DimensionInput name='height' label='height' />
+      <Select
+        name='justifyContent'
+        label='Horizontal alignment'
+        defaultValue='start'
+        options={['center', 'start', 'end']}
+      />
+      <Select
+        name='alignItems'
+        label='Vertical alignment'
+        defaultValue='start'
+        options={['center', 'start', 'end']}
+      />
+      <Select
+        name='textAlign'
+        label='Text alignment'
+        defaultValue='initial'
+        options={['initial', 'center', 'justify', 'start', 'end']}
+      />
+      <Input label='Color' name='color' type='color' />
+      <Select
+        label='Font weight'
+        name='fontWeight'
+        defaultValue='normal'
+        options={['normal', 'bolder', 'bold', 'lighter']}
+      />
+      <DimensionInput label='Font size' name='fontSize' />
+    </Form>
+  );
+};
--- src/modules/templates/pages/builderPage/index.tsx
+++ src/modules/templates/pages/builderPage/index.tsx
@@ -1,15 +1,10 @@
 import { CSSProperties, useEffect, useState } from 'react';
-import { BackgroundInput } from '../../../form/BackgroundInput';
-import { DimensionInput } from '../../../form/DimensionInput';
-import { FontSelect } from '../../../form/FontSelector';
-import Form from '../../../form/Form';
-import { Input } from '../../../form/Input';
-import { Select } from '../../../form/Select';
 import { API_ROUTES, ROUTES } from '../../../shared/routes/ROUTES';
 import { stringHelper } from '../../../shared/services/stringHelper';
 import { Template, TextItemProps } from '../../models/template.model';
 import { DownloadFonts } from '../../shared/DonwloadFonts';
 import { ActionBar } from './components/ActionBar';
+import { ParametersForm } from './components/ParametersForm';
 import { PageContextProvider } from './PageContext';
 import { ResultDesign } from './ResultDesign';
 
@@ -183,105 +178,5 @@ export const BuilderPage = ({
         )} */}
       </div>
     </PageContextProvider>
-  );
-};
-
-const containerStyle: CSSProperties = {
-  display: 'flex',
-  flexDirection: 'column',
-  maxWidth: 250,
-  padding: 20,
-  border: '1px solid grey',
-};
-const ParametersForm = ({
-  defaultValues,
-  onSubmit,
-}: {
-  defaultValues?;
-  onSubmit: (data) => void;
-}) => {
-  return (
-    <Form<Template['page']>
-      defaultValues={defaultValues}
-      onSubmit={(data) => {
-        onSubmit(data);
-      }}
-      style={containerStyle}
-    >
-      <h4 style={{ marginTop: 0 }}>Page parameters</h4>
-      <DimensionInput name='width' label='width' />
-      {/* <Input name='width' label='width'></Input> */}
-      <DimensionInput name='height' label='height' />
-
-      <BackgroundInput name='background' />
-
-      <Input label='Background color' name='backgroundColor' type='color' />
-      <Select
-        label='Background size'
-        name='backgroundSize'
-        options={['auto', 'contain', 'cover']}
-      />
-      <Select
-        label='Background repeat'
-        name='backgroundRepeat'
-        options={['no-repeat', 'repeat-x', 'repeat-y', 'round']}
-      />
-      <Select
-        label='Background size'
-        name='backgroundPosition'
-        options={['center', 'left', 'right', 'top', 'bottom', 'unset']}
-      />
-      {/* <Input name='height' label='height'></Input> */}
-    </Form>
-  );
-};
-const TextInputFormFields = ({
-  defaultValues,
-  onSubmit,
-}: {
-  defaultValues?;
-  onSubmit: (data) => void;
-}) => {
-  return (
-    <Form
-      defaultValues={defaultValues}
-      onChange={({ top, left, ...data }) => {
-        onSubmit(data);
-      }}
-      onSubmit={({ top, left, ...data }) => {
-        onSubmit(data);
-      }}
-      style={{ display: 'flex', flexDirection: 'column', maxWidth: 200 }}
-    >
-      <FontSelect name='fontFamily' label='font' />
-      <DimensionInput name='width' label='width' />
-      <DimensionInput name='height' label='height' />
-      <Select
-        name='justifyContent'
-        label='Horizontal alignment'
-        defaultValue='start'
-        options={['center', 'start', 'end']}
-      />
-      <Select
-        name='alignItems'
-        label='Vertical alignment'
-        defaultValue='start'
-        options={['center', 'start', 'end']}
-      />
-      <Select
-        name='textAlign'
-        label='Text alignment'
-        defaultValue='initial'
-        options={['initial', 'center', 'justify', 'start', 'end']}
-      />
-      <Input label='Color' name='color' type='color' />
-      <Select
-        label='Font weight'
-        name='fontWeight'
-        defaultValue='normal'
-        options={['normal', 'bolder', 'bold', 'lighter']}
-      />
-      <DimensionInput label='Font size' name='fontSize' />
-    </Form>
   );
 };
