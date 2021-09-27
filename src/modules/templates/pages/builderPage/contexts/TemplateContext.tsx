c8b16c6e363af63544636e820455a22cfddd0f67
Daniel Tchangang <daniel.tchangang@gmail.com>
2021-09-27 12:21:54 +0200

    feat: move TemplateContext to contexts folder

---
+++ src/modules/templates/pages/builderPage/contexts/TemplateContext.tsx
@@ -0,0 +1,107 @@
+import { createContext, CSSProperties, useEffect, useState } from 'react';
+import { Template, TextItemProps } from '../../../models/template.model';
+import { DownloadFonts } from '../../../shared/DonwloadFonts';
+
+export const TemplateContext = createContext<
+  TemplateContextData & TemplateContextAction
+>(null);
+type TemplateContextData = { state: Template; itemToUpdate: string };
+type TemplateContextAction = {
+  updatePageData: (pageData: Partial<Template['page']>) => void;
+  updateElement: (
+    elementId: string,
+    update: Partial<{ value: string; style: CSSProperties }>,
+  ) => void;
+  createNewElement: (elementProps: unknown) => void;
+  deleteElement: (elementId?: string) => void;
+  setItemToUpdate: (elementId?: string) => void;
+};
+export const TemplateContextProvider = ({
+  children,
+  initialTemplate,
+  onChange,
+}: {
+  initialTemplate: Template;
+  children: React.ReactNode;
+  onChange?: (newState: Template) => void;
+}) => {
+  const [state, setTemplate] = useState<Template>(initialTemplate);
+  const [itemToUpdate, setItemToUpdate] = useState<string>(null);
+  const [fontFamilyRequest, setFontFamilyRequest] = useState(null);
+  useEffect(() => {
+    const template = new Template(initialTemplate);
+    setFontFamilyRequest(template.getGoogleRequestForFonts());
+  }, [initialTemplate]);
+
+  const updateState = (newState: Template) => {
+    setTemplate({ ...state, ...newState });
+    onChange?.(newState);
+  };
+
+  const updatePageData = (newPageProps) => {
+    const newState = { ...state, page: { ...state.page, ...newPageProps } };
+    updateState(newState);
+  };
+
+  const updateElement = (
+    itemId: string,
+    update: Partial<{ value: string; style: CSSProperties }>,
+  ) => {
+    const elementToUpdate = state.elements[itemId];
+    updateState({
+      ...state,
+      elements: {
+        ...state.elements,
+        [itemId]: {
+          ...elementToUpdate,
+          value: update.value || elementToUpdate.value,
+          style: {
+            ...elementToUpdate.style,
+            ...update.style,
+          },
+        } as TextItemProps,
+      },
+    });
+  };
+
+  const createNewElement = (newElement: TextItemProps) => {
+    updateState({
+      ...state,
+      elements: {
+        ...state.elements,
+        [newElement.id]: newElement,
+      },
+    });
+  };
+
+  const deleteElement = (elementId = itemToUpdate) => {
+    if (!elementId || !state.elements[elementId]) {
+      return;
+    }
+    const allElements = { ...state.elements };
+
+    delete allElements[elementId];
+    updateState({
+      ...state,
+      elements: allElements,
+    });
+    setItemToUpdate(null);
+  };
+
+  return (
+    <TemplateContext.Provider
+      value={{
+        state: state,
+        itemToUpdate,
+        updatePageData,
+        updateElement,
+        createNewElement,
+        deleteElement,
+        setItemToUpdate,
+      }}
+    >
+      <DownloadFonts fontFamilyRequest={fontFamilyRequest} />
+      {children}
+    </TemplateContext.Provider>
+  );
+};
--- src/modules/templates/pages/builderPage/index.tsx
+++ src/modules/templates/pages/builderPage/index.tsx
@@ -1,15 +1,12 @@
-import {
-  createContext,
-  CSSProperties,
-  useContext,
-  useEffect,
-  useState,
-} from 'react';
-import { Template, TextItemProps } from '../../models/template.model';
-import { DownloadFonts } from '../../shared/DonwloadFonts';
+import { useContext } from 'react';
+import { Template } from '../../models/template.model';
 import { ActionBar } from './components/ActionBar';
 import { ParametersForm } from './components/ParametersForm';
 import { QueryAndDownloadUrls } from './components/QueryAndDownloadUrls';
+import {
+  TemplateContext,
+  TemplateContextProvider,
+} from './contexts/TemplateContext';
 import { PageContextProvider } from './PageContext';
 import { ResultDesign } from './ResultDesign';
 
@@ -73,110 +70,5 @@ const BuilderPageContent = () => {
         <QueryAndDownloadUrls state={state} />
       </div>
     </div>
-  );
-};
-
-type TemplateContextData = { state: Template; itemToUpdate: string };
-type TemplateContextAction = {
-  updatePageData: (pageData: Partial<Template['page']>) => void;
-  updateElement: (
-    elementId: string,
-    update: Partial<{ value: string; style: CSSProperties }>,
-  ) => void;
-  createNewElement: (elementProps: unknown) => void;
-  deleteElement: (elementId?: string) => void;
-  setItemToUpdate: (elementId?: string) => void;
-};
-const TemplateContext = createContext<
-  TemplateContextData & TemplateContextAction
->(null);
-
-const TemplateContextProvider = ({
-  children,
-  initialTemplate,
-  onChange,
-}: {
-  initialTemplate: Template;
-  children: React.ReactNode;
-  onChange?: (newState: Template) => void;
-}) => {
-  const [state, setTemplate] = useState<Template>(initialTemplate);
-  const [itemToUpdate, setItemToUpdate] = useState<string>(null);
-  const [fontFamilyRequest, setFontFamilyRequest] = useState(null);
-  useEffect(() => {
-    const template = new Template(initialTemplate);
-    setFontFamilyRequest(template.getGoogleRequestForFonts());
-  }, [initialTemplate]);
-
-  const updateState = (newState: Template) => {
-    setTemplate({ ...state, ...newState });
-    onChange?.(newState);
-  };
-
-  const updatePageData = (newPageProps) => {
-    const newState = { ...state, page: { ...state.page, ...newPageProps } };
-    updateState(newState);
-  };
-
-  const updateElement = (
-    itemId: string,
-    update: Partial<{ value: string; style: CSSProperties }>,
-  ) => {
-    const elementToUpdate = state.elements[itemId];
-    updateState({
-      ...state,
-      elements: {
-        ...state.elements,
-        [itemId]: {
-          ...elementToUpdate,
-          value: update.value || elementToUpdate.value,
-          style: {
-            ...elementToUpdate.style,
-            ...update.style,
-          },
-        } as TextItemProps,
-      },
-    });
-  };
-
-  const createNewElement = (newElement: TextItemProps) => {
-    updateState({
-      ...state,
-      elements: {
-        ...state.elements,
-        [newElement.id]: newElement,
-      },
-    });
-  };
-
-  const deleteElement = (elementId = itemToUpdate) => {
-    if (!elementId || !state.elements[elementId]) {
-      return;
-    }
-    const allElements = { ...state.elements };
-
-    delete allElements[elementId];
-    updateState({
-      ...state,
-      elements: allElements,
-    });
-    setItemToUpdate(null);
-  };
-
-  return (
-    <TemplateContext.Provider
-      value={{
-        state: state,
-        itemToUpdate,
-        updatePageData,
-        updateElement,
-        createNewElement,
-        deleteElement,
-        setItemToUpdate,
-      }}
-    >
-      <DownloadFonts fontFamilyRequest={fontFamilyRequest} />
-      {children}
-    </TemplateContext.Provider>
   );
 };
