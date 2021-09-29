2c7e1ced7901e46d20cad17341b4f694917a6b45
Daniel Tchangang <daniel.tchangang@gmail.com>
2021-09-29 10:36:38 +0200

    feat: update uploadFileComponent to allow direct update

--- src/modules/components/uploadFileComponent.tsx
+++ src/modules/components/uploadFileComponent.tsx
@@ -5,9 +5,11 @@ import { useAsyncFn } from 'react-use';
 export const UploadFileComponent = ({
   url,
   onChange,
+  withValidation,
 }: {
   url: string;
   onChange: (newValue: string) => void;
+  withValidation?: boolean;
 }) => {
   const [file, setFile] = useState<File>(null);
   const [uploadFileState, uploadFileFn] = useAsyncFn(
@@ -35,27 +37,39 @@ export const UploadFileComponent = ({
     setImageUrl(null);
     onChange(null);
   };
+  const uploadFile = (file: File) => {
+    if (file) {
+      const data = new FormData();
+      data.append('file', file);
+      uploadFileFn(data);
+    }
+  };
   return (
     <div>
       {!imageUrl ? (
         <div style={{ display: 'flex', flexDirection: 'column' }}>
           <input
             type='file'
-            onChange={(event) => setFile(event.target.files[0])}
-          />
-          <button
-            style={{ maxWidth: 100 }}
-            onClick={() => {
-              if (file) {
-                const data = new FormData();
-                data.append('file', file);
-                uploadFileFn(data);
+            onChange={(event) => {
+              const fileToUpload = event.target.files[0];
+              if (withValidation) {
+                setFile(fileToUpload);
+              } else {
+                uploadFile(fileToUpload);
               }
             }}
-          >
-            {uploadFileState.loading && 'loading'}
-            Valider
-          </button>
+          />
+          {withValidation && (
+            <button
+              style={{ maxWidth: 100 }}
+              onClick={() => {
+                uploadFile(file);
+              }}
+            >
+              {uploadFileState.loading && 'loading'}
+              Valider
+            </button>
+          )}
         </div>
       ) : (
         <ImagePreview url={imageUrl} onDelete={() => onDeleteImage()} />
@@ -75,8 +89,8 @@ const ImagePreview = ({
     return null;
   }
   return (
-    <div style={{ position: 'relative' }}>
-      <Image src={url} width={200} height={200} objectFit='cover' />
+    <div style={{ position: 'relative', }}>
+      <Image src={url} width={200} height={200} objectFit='contain' />
       <button
         onClick={() => onDelete()}
         style={{ position: 'absolute', top: 10, left: 10 }}
