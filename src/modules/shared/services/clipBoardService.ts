58730e9314d09489730a7a266f8a6ce9fb90f76f
Daniel Tchangang <daniel.tchangang@gmail.com>
2021-10-03 19:26:17 +0200

    feat: add retrieveImageFromClipboardAsBlob in clipboardService

--- src/modules/shared/services/clipBoardService.ts
+++ src/modules/shared/services/clipBoardService.ts
@@ -1,6 +1,33 @@
+import { ClipboardEvent } from 'react';
+
 export const clipBoardService = {
   copy: (data) => {
     const dataToSave = typeof data === 'string' ? data : JSON.stringify(data);
+    console.log(dataToSave);
     navigator.clipboard.writeText(JSON.stringify(dataToSave));
+  },
+
+  // Handle the event
+  retrieveImageFromClipboardAsBlob: async (pasteEvent: ClipboardEvent) => {
+    if (!pasteEvent.clipboardData) {
+      return null;
+    }
+
+    var items = pasteEvent.clipboardData.items;
+
+    if (items == undefined) {
+      return null;
+    }
+    console.log('here');
+
+    for (var i = 0; i < items.length; i++) {
+      // Skip content if not image
+      if (items[i].type.indexOf('image') == -1) continue;
+      // Retrieve image on clipboard as blob
+      console.log('found file');
+      var blob = items[i].getAsFile();
+
+      return blob;
+    }
   },
 };
