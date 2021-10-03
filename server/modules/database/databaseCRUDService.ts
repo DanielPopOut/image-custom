c54a3883fd0078cca5381a2c018f4c1bb64d461c
Daniel Tchangang <daniel.tchangang@gmail.com>
2021-10-03 20:38:06 +0200

    fix: improve typing on update _id type

--- server/modules/database/databaseCRUDService.ts
+++ server/modules/database/databaseCRUDService.ts
@@ -27,7 +27,7 @@ export class DataBaseCrudService<T> {
   };
 
   updateOne = async (
-    _id,
+    _id: string | ObjectId,
     { _id: _removed, ...data }: Partial<T> & { _id: string },
     upsert?: boolean,
   ) => {
@@ -41,7 +41,7 @@ export class DataBaseCrudService<T> {
   };
 
   updateOneFullAction = async (
-    _id,
+    _id: string | ObjectId,
     updateData: UpdateFilter<T>,
     upsert?: boolean,
   ) => {
