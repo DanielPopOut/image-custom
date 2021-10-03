53fd8bc3c88385a4a862e39bf6e32b89b130ce2c
Daniel Tchangang <daniel.tchangang@gmail.com>
2021-10-03 16:53:23 +0200

    feat: create EmptyDiv component

---
+++ src/modules/shared/components/EmptyDiv/EmptyDivBoxIllustration.tsx
@@ -0,0 +1,22 @@
+import { HTMLAttributes } from 'react';
+
+export const EmptyDivBoxIllustration = ({
+  style,
+  ...props
+}: HTMLAttributes<HTMLDivElement>) => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        ...style,
+      }}
+      {...props}
+    >
+      <img src='/emptyBox.svg' style={{ width: '100%', height: '100%' }} />
+      <a href='https://storyset.com/work'>Work illustrations by Storyset</a>
+    </div>
+  );
+};
