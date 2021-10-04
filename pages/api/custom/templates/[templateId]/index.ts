d3595d2ec61033e4a9509aee7a3a8eb25f72c085
Daniel Tchangang <daniel.tchangang@gmail.com>
2021-10-04 10:03:35 +0200

    feat: use custom route to get template preview

---
+++ pages/api/custom/templates/[templateId]/index.ts
@@ -0,0 +1,17 @@
+import { templateService } from 'server/modules/templates/template.service';
+import {
+  ApiResponseError,
+  ApiResponseSuccess,
+} from 'server/shared/ApiResponseFormat';
+
+export default async (req, res) => {
+  const elementId = req.query.templateId as string;
+  try {
+    const elementFound = await templateService.findOneById(elementId);
+    console.log({ elementId, elementFound });
+    res.json(ApiResponseSuccess(elementFound));
+  } catch (e) {
+    console.error(e);
+    res.status(400).json(ApiResponseError(e.message));
+  }
+};
